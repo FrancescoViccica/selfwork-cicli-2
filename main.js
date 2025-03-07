fb3c2d2ed44669db5446e96dd4b67e6559fbc123
Francesco Viccica <viccicafrancesco@gmail.com>
2025-03-07 15:16:00 +0100

    Esercizio svolto

---
+++ main.js
@@ -0,0 +1,17 @@
+let somma_dis = 0;
+
+
+for( let i = 1; i <= 20; i++){
+    if (i % 2 ==0){
+        console.log(i);
+        
+    }else{
+        somma_dis += i
+        
+    }
+}
+
+console.log(`la somma dei numeri dispari è ${somma_dis}`);
+
+
+console.log(`La media dei numeri dispari è ${somma_dis/10}`);
